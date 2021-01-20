02dd308df501f4d2a563cdd550ac930c5d78fd06
Barış Özer <barisozer94@gmail.com>
2021-01-20 20:30:16 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Bilgileri Eklenmiştir.");
+        }
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi+" Bilgileri Listelenmiştir.");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Bilgileri Silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Barış";
+            musteri1.MusteriSoyadi = "Özer";
+            musteri1.MusteriTel = 0531701556;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Atakan";
+            musteri2.MusteriSoyadi = "İlci";
+            musteri2.MusteriTel = 0531701889;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriListeleme(musteri1);
+            musteriManager.MusteriSilme(musteri2);
+        }
+    }
+}
